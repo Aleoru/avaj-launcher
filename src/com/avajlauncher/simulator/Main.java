a606b70474e8cbfb17511d7abf4422db63481216
Alejandra Oropeza <57480716+Aleoru@users.noreply.github.com>
2025-11-11 13:40:57 +0100

    Fix Main.java

--- src/com/avajlauncher/simulator/Main.java
+++ src/com/avajlauncher/simulator/Main.java
@@ -4,40 +4,18 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
-        File scenario = new File(args[0]);
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		File scenario = new File(args[0]);
 
-        try (Scanner myReader = new Scanner(scenario)) {
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error ocurred");
-            e.printStackTrace();
-        }
-    }
-}
-
-package com.avajlauncher.simulator;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
-        File scenario = new File(args[0]);
-
-        try (Scanner myReader = new Scanner(scenario)) {
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error ocurred");
-            e.printStackTrace();
-        }
-    }
+		try (Scanner myReader = new Scanner(scenario)) {
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("An error ocurred");
+			e.printStackTrace();
+		}
+	}
 }
