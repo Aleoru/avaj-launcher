3898f1da8c64ba7fc2f6678d2adb5a2d816c3b22
Alejandra Oropeza <57480716+Aleoru@users.noreply.github.com>
2025-12-12 14:13:24 +0100

    Update WeatherProvider.java
    Added to getCurrentWeather method functionality, it receives a coordinates and returns weather on that specified area

--- src/com/avaj/weather/WeatherProvider.java
+++ src/com/avaj/weather/WeatherProvider.java
@@ -1,5 +1,33 @@
 package com.avaj.weather;
+import com.avaj.aircraft.Coordinates;
 
+/**
+ * 
+ */
 public class WeatherProvider {
+	private static WeatherProvider provider = new WeatherProvider();
 
+	private static String[] weather = {"SUN", "RAIN", "FOG", "SNOW"};
+
+	private WeatherProvider() {}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static WeatherProvider getProvider(){
+		return WeatherProvider.provider;
+	}
+
+	/** */
+	public String getCurrentWeather(Coordinates p_coordinates){
+		int seed = p_coordinates.getLongitude() + p_coordinates.getLatitude() + p_coordinates.getHeight();
+
+		int index = (seed + p_coordinates.getHeight()) % 4;
+
+		if (index < 0) {
+			index += 4;
+		}
+		return weather[index];
+	}
 }
