baa74fe68dbd25cfca2605d19efb330e39212f7a
aleoru0022 <aleoru0022@gmail.com>
2025-12-13 10:55:07 +0000

    Added comments to weather Provider and finished it

--- src/com/avaj/weather/WeatherProvider.java
+++ src/com/avaj/weather/WeatherProvider.java
@@ -2,7 +2,7 @@ package com.avaj.weather;
 import com.avaj.aircraft.Coordinates;
 
 /**
- * 
+ * Singleton WeatherProvider. Provides the weather based on the coordinates.
  */
 public class WeatherProvider {
 	private static WeatherProvider provider = new WeatherProvider();
@@ -11,15 +11,15 @@ public class WeatherProvider {
 
 	private WeatherProvider() {}
 
-	/**
-	 * 
-	 * @return
-	 */
 	public static WeatherProvider getProvider(){
 		return WeatherProvider.provider;
 	}
 
-	/** */
+	/**
+	 * Determines the weather based on the coordinates.
+	 * @param p_coordinates Aircraft coordinates.
+	 * @return A string with the weather ("SUN", "RAIN", "FOG", "SNOW")
+	 */
 	public String getCurrentWeather(Coordinates p_coordinates){
 		int seed = p_coordinates.getLongitude() + p_coordinates.getLatitude() + p_coordinates.getHeight();
 
